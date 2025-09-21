84e642859da63d6f40ac3e30cebf80e4fdd5ccb1
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-09-21 14:33:46 +0700

    feat: implement search bar (belum integrasi api)

--- app/src/main/java/com/example/kelompokgminiproject/MainActivity.kt
+++ app/src/main/java/com/example/kelompokgminiproject/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.kelompokgminiproject
 
 import android.os.Bundle
+import android.widget.SearchView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,44 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val searchView = findViewById<SearchView>(R.id.searchView)
+
+        // Default -> tampilakn welcome fragmentynya
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, WelcomeFragment())
+            .commit()
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (!query.isNullOrEmpty()) {
+                    val fragment = SearchResultFragment().apply {
+                        arguments = Bundle().apply {
+                            putString("query", query)
+                        }
+                    }
+
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainer, fragment)
+                        .commit()
+                } else {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainer, WelcomeFragment())
+                        .commit()
+                }
+
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText.isNullOrEmpty()) {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainer, WelcomeFragment())
+                        .commit()
+                }
+
+                return true
+            }
+        })
     }
 }
